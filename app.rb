08c491b0a0c6c7de7d5b6961cded5b22c702fc04
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-25 23:55:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
-  "Hello, World!"
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mina"
+  end
+
+  get '/hometown' do
+    "My hometown is Culver City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Drive Slow"
   end
 end
